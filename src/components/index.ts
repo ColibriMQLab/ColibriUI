a822068b15020699c3835f2ed350eae99b6b401f
Sergey Bocharov <sergey.bocharov@emergingtravel.com>
2024-05-18 08:11:44 +0100

    Add Grid component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 export { default as Button } from "./Button";
+export { default as Checkbox } from "./Checkbox";
 export { default as DotsLoader } from "./DotsLoader";
+export { default as Grid } from "./Grid";
 export { default as Switch } from "./Switch";
 export { THEMES, ThemeProvider } from "./Theme";
 export { default as Typography } from "./Typography";
