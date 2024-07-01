33c105081a16a8ee6d1d03cf09906def440673ce
Sergey Bocharov <Sergei-b84@bk.ru>
2024-07-01 18:10:10 +0100

    Fix main file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
-export { default as Separator } from "./Separator";
+export { default as DotsLoader } from "./DotsLoader";
 export { default as Grid } from "./Grid";
 export { default as GridItem } from "./Grid/Item";
-export { default as Typography } from "./Typography";
+export { default as Separator } from "./Separator";
 export { THEMES } from "./Theme";
+export { default as Typography } from "./Typography";
