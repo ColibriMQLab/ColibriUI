e86eeab71fb97e6f017ef0a1a60531d45ba4a308
Sergey Bocharov <Sergei-b84@bk.ru>
2024-07-01 22:40:29 +0100

    Add Input

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,8 @@
 export { default as DotsLoader } from "./DotsLoader";
 export { default as Grid } from "./Grid";
 export { default as GridItem } from "./Grid/Item";
+export * from "./Icons";
+export { default as Input } from "./Input";
 export { default as Separator } from "./Separator";
 export { THEMES } from "./Theme";
 export { default as Typography } from "./Typography";
-export * from "./Icons";
