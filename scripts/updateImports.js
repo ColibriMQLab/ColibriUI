7a7ef2c7e890c236e96a2bdb6eeb33692286abe3
Sergey Bocharov <Sergei-b84@bk.ru>
2024-07-10 18:17:02 +0100

    Add new build

--- scripts/updateImports.js
+++ scripts/updateImports.js
@@ -1,4 +1,4 @@
-const { promises } = require("fs");
+const { writeFile, readFile } = require('fs-extra');
 const fg = require("fast-glob");
 
 const inputDir = 'dist';
@@ -7,9 +7,9 @@ async function updateTsImports() {
   const files = await fg(`${inputDir}/**/*.js`);
 
   for (const file of files) {
-    const jsFileContent = await promises.readFile(file, 'utf-8');
-    const updatedTsFileContent = jsFileContent.replace(/\.scss/g, '.css');
-    await promises.writeFile(file, updatedTsFileContent);
+    const fileContent = await readFile(file, 'utf-8');
+    const updatedTsFileContent = fileContent.replace(/\.scss/g, '.css');
+    await writeFile(file, updatedTsFileContent);
   }
 }
 
