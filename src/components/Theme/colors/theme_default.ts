6bb2de96f53b7539c02222aa94c71340c2eda5de
Sergey Bocharov <Sergei-b84@bk.ru>
2024-09-02 19:04:00 +0100

    Fix Theme

--- src/components/Theme/colors/theme_default.ts
+++ src/components/Theme/colors/theme_default.ts
@@ -1,25 +1,25 @@
 export const Primary = {
-  PRIMARY_1: "#E8F0FE",
-  PRIMARY_2: "#B8D2FF",
-  PRIMARY_3: "#73ABF8",
-  PRIMARY_4: "#067AFF",
-  PRIMARY_5: "#0066DC",
+  PRIMARY_DEFAULT_1: "#E8F0FE",
+  PRIMARY_DEFAULT_2: "#B8D2FF",
+  PRIMARY_DEFAULT_3: "#73ABF8",
+  PRIMARY_DEFAULT_4: "#067AFF",
+  PRIMARY_DEFAULT_5: "#0066DC",
 };
 
 export const Secondary = {
-  SECONDARY_1: "#fff1db",
-  SECONDARY_2: "#ffdaa3",
-  SECONDARY_3: "#ffca79",
-  SECONDARY_4: "#f4b968",
-  SECONDARY_5: "#ffab0f",
+  SECONDARY_DEFAULT_1: "#fff1db",
+  SECONDARY_DEFAULT_2: "#ffdaa3",
+  SECONDARY_DEFAULT_3: "#ffca79",
+  SECONDARY_DEFAULT_4: "#f4b968",
+  SECONDARY_DEFAULT_5: "#ffab0f",
 };
 
 export const Tretiary = {
-  TRETIARY_1: "#FFF5F0",
-  TRETIARY_2: "#FFEAE2",
-  TRETIARY_3: "#FFD8CA",
-  TRETIARY_4: "#FFA384 ",
-  TRETIARY_5: "#fc5200",
+  TRETIARY_DEFAULT_1: "#FFF5F0",
+  TRETIARY_DEFAULT_2: "#FFEAE2",
+  TRETIARY_DEFAULT_3: "#FFD8CA",
+  TRETIARY_DEFAULT_4: "#FFA384 ",
+  TRETIARY_DEFAULT_5: "#fc5200",
 };
 
 export const Brand = {
