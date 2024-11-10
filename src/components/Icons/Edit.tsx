7ba916328fc2d8864b274a7565bfc81ac87a9de7
Sergey Bocharov <Sergei-b84@bk.ru>
2024-11-10 09:47:11 +0000

    Fix Edit icon

--- src/components/Icons/Edit.tsx
+++ src/components/Icons/Edit.tsx
@@ -7,6 +7,9 @@ export default forwardRef<SVGSVGElement, Props>(
       xmlns="http://www.w3.org/2000/svg"
       width={width}
       height={height}
+      {...props}
+      ref={ref}
+      viewBox="0 0 24 24"
       fill="none"
     >
       <path
