983c9a56dc7369a9b9b0d59e7dee4e1393775959
Sergey Bocharov <Sergei-b84@bk.ru>
2025-10-11 18:58:02 +0100

    Add React memo to Month Item

--- src/components/Calendar/components/Month/Month.tsx
+++ src/components/Calendar/components/Month/Month.tsx
@@ -3,8 +3,8 @@ import classNames from "classnames/bind";
 import { leadingZeros } from "../../../libs/numbers/leadingZeros";
 import Typography from "../../../Typography";
 import {
-  getShortWeekDayNameByIndex,
-  getUpperMonthName,
+	getShortWeekDayNameByIndex,
+	getUpperMonthName,
 } from "../../../helpers/date";
 import { getMonthWeeks } from "../../utils/getMonthWeeks";
 import styles from "./index.module.scss";
@@ -13,160 +13,150 @@ import type { FunctionComponent, PropsWithChildren } from "react";
 
 const clx = classNames.bind(styles);
 // eslint-disable-next-line @typescript-eslint/ban-types
-export interface FCWithElements<T = {}> extends FunctionComponent<T> {}
+export interface FCWithElements<T = {}> extends FunctionComponent<T> { }
 
 type Props = {
-  today: string;
-  startDate: Date;
-  activeDays?: string[];
-  selectedDate?: string;
-  selectedPeriod?: number;
-  onDayClick: (date: string) => void;
-  offsetLeft: number;
-  availableDates?: string[];
-  titleSize?: CalendarTitleSize;
+	today: string;
+	startDate: Date;
+	activeDays?: string[];
+	selectedDate?: string;
+	selectedPeriod?: number;
+	onDayClick: (date: string) => void;
+	offsetLeft: number;
+	availableDates?: string[];
+	titleSize?: CalendarTitleSize;
 };
 
 type ComponentProps = {
-  isStartOfSelection?: boolean;
-  isEndOfSelection?: boolean;
-  isSelected?: boolean;
-  isInSelectedRange?: boolean;
+	isStartOfSelection?: boolean;
+	isEndOfSelection?: boolean;
+	isSelected?: boolean;
+	isInSelectedRange?: boolean;
 };
 
 const Item = React.memo(({
-  isSelected,
-  isInSelectedRange,
-  isStartOfSelection,
-  isEndOfSelection,
-  children,
+	isSelected,
+	isInSelectedRange,
+	isStartOfSelection,
+	isEndOfSelection,
+	children,
 }: PropsWithChildren<ComponentProps>) => {
-  const noSelection =
-    isStartOfSelection && isEndOfSelection ? styles["no-selection"] : "";
-  const startClass =
-    isStartOfSelection && !isEndOfSelection ? styles["start-of-selection"] : "";
-  const endClass =
-    isEndOfSelection && !isStartOfSelection ? styles["end-of-selection"] : "";
-  const firstChildClass = isEndOfSelection ? styles["first-child"] : "";
-  const lastChildClass = isStartOfSelection ? styles["last-child"] : "";
+	const itemClass = clx('day', {
+		["selected-day"]: isSelected,
+		["selected-range-day"]: isInSelectedRange || isSelected,
+		["no-selection"]: isStartOfSelection && isEndOfSelection,
+		["start-of-selection"]: isStartOfSelection && !isEndOfSelection,
+		["end-of-selection"]: isEndOfSelection && !isStartOfSelection,
+		["first-child"]: isEndOfSelection,
+		["last-child"]: isStartOfSelection,
+	});
 
-  if (isSelected) {
-    return (
-      <li
-        className={clx(
-          styles["selected-range-day"],
-          styles["selected-day"],
-          noSelection,
-          startClass,
-          endClass,
-          firstChildClass,
-          lastChildClass,
-        )}
-      >
-        {children}
-      </li>
-    );
-  } else if (isInSelectedRange) {
-    return <li className={clx(styles["selected-range-day"])}>{children}</li>;
-  }
+	if (isSelected) {
+		return (
+			<li className={itemClass}>
+				{children}
+			</li>
+		);
+	} else if (isInSelectedRange) {
+		return <li className={clx("selected-range-day")}>{children}</li>;
+	}
 
-  return <li className={clx(styles.day)}>{children}</li>;
+	return <li className={clx('day')}>{children}</li>;
 });
 
 export const Month: FCWithElements<Props> = (props) => {
-  const year = props.startDate.getFullYear();
-  const month = leadingZeros(props.startDate.getMonth() + 1, 2);
+	const year = props.startDate.getFullYear();
+	const month = leadingZeros(props.startDate.getMonth() + 1, 2);
 
-  return (
-    <div
-      data-testid="month"
-      className={clx(styles.root)}
-      style={{ left: props.offsetLeft ? `${props.offsetLeft}px` : undefined }}
-    >
-      <div className={clx(styles["month-name"])}>
-        <Typography tag="span" size={props.titleSize}>
-          {getUpperMonthName(props.startDate)}
-        </Typography>
-      </div>
-      <div className={clx(styles.table)}>
-        <ul className={clx(styles.legend)}>
-          {[...Array(7)].map((_, ...rest) => {
-            const index = rest[0] + 1;
+	return (
+		<div
+			data-testid="month"
+			className={clx('root')}
+			style={{ left: props.offsetLeft ? `${props.offsetLeft}px` : undefined }}
+		>
+			<div className={clx("month-name")}>
+				<Typography tag="span" size={props.titleSize}>
+					{getUpperMonthName(props.startDate)}
+				</Typography>
+			</div>
+			<div className={clx('table')}>
+				<ul className={clx('legend')}>
+					{[...Array(7)].map((_, ...rest) => {
+						const index = rest[0] + 1;
+						return (
+							<li className={clx('day')} key={index}>
+								<Typography
+									style={{
+										opacity: index > 5 ? 80 : 30,
+										color:
+											index > 5
+												? "var(--typography-alert)"
+												: "var(--palette-black)",
+									}}
+								>
+									{getShortWeekDayNameByIndex(index)}
+								</Typography>
+							</li>
+						);
+					})}
+				</ul>
+				{getMonthWeeks(props).map((days, ...rest1) => (
+					<ul key={`week-${rest1[0]}`} className={clx('week')}>
+						{days.map((day, ...rest2) => {
+							const key = `day-${rest2[0]}`;
 
-            return (
-              <li className={clx(styles.day)} key={index}>
-                <Typography
-                  style={{
-                    opacity: index > 5 ? 80 : 30,
-                    color:
-                      index > 5
-                        ? "var(--typography-alert)"
-                        : "var(--palette-black)",
-                  }}
-                >
-                  {getShortWeekDayNameByIndex(index)}
-                </Typography>
-              </li>
-            );
-          })}
-        </ul>
-        {getMonthWeeks(props).map((days, ...rest1) => (
-          <ul key={`week-${rest1[0]}`} className={clx(styles.week)}>
-            {days.map((day, ...rest2) => {
-              const key = `day-${rest2[0]}`;
+							if (day.isDummy) {
+								return <li className={clx('day')} key={key} />;
+							}
+							const attrs = {
+								isStartOfSelection: day.isStartOfSelection,
+								isEndOfSelection: day.isEndOfSelection,
+								isSelected: day.isSelected,
+								isInSelectedRange: day.isInSelectedRange,
+							};
 
-              if (day.isDummy) {
-                return <li className={clx(styles.day)} key={key} />;
-              }
-              const attrs = {
-                isStartOfSelection: day.isStartOfSelection,
-                isEndOfSelection: day.isEndOfSelection,
-                isSelected: day.isSelected,
-                isInSelectedRange: day.isInSelectedRange,
-              };
+							const dayInnerActive = day.isActive
+								? "day-inner-active"
+								: "";
 
-              const dayInnerActive = day.isActive
-                ? styles["day-inner-active"]
-                : "";
-
-              return (
-                <Item {...attrs} key={key}>
-                  <button
-                    className={clx(styles["day-inner"], dayInnerActive)}
-                    style={{
-                      background: day.isSelected
-                        ? "var(--palette-bg-2)"
-                        : "transparent",
-                    }}
-                    role="button"
-                    type="button"
-                    onClick={
-                      day.isActive
-                        ? () =>
-                            props.onDayClick(
-                              [year, month, leadingZeros(day.day, 2)].join("-"),
-                            )
-                        : undefined
-                    }
-                  >
-                    <Typography
-                      size="s"
-                      style={{
-                        opacity: day.isActive ? 80 : 30,
-                        color: day.isActive
-                          ? "rgba(0, 0, 0, 0.8)"
-                          : "rgba(0, 0, 0, 0.3)",
-                      }}
-                    >
-                      {day.day}
-                    </Typography>
-                  </button>
-                </Item>
-              );
-            })}
-          </ul>
-        ))}
-      </div>
-    </div>
-  );
+							return (
+								<Item {...attrs} key={key}>
+									<button
+										className={clx("day-inner", dayInnerActive)}
+										style={{
+											background: day.isSelected
+												? "var(--palette-bg-2)"
+												: "transparent",
+										}}
+										role="button"
+										type="button"
+										onClick={
+											day.isActive
+												? () =>
+													props.onDayClick(
+														[year, month, leadingZeros(day.day, 2)].join("-"),
+													)
+												: undefined
+										}
+									>
+										<Typography
+											size="s"
+											style={{
+												opacity: day.isActive ? 80 : 30,
+												color: day.isActive
+													? "rgba(0, 0, 0, 0.8)"
+													: "rgba(0, 0, 0, 0.3)",
+											}}
+										>
+											{day.day}
+										</Typography>
+									</button>
+								</Item>
+							);
+						})}
+					</ul>
+				))}
+			</div>
+		</div>
+	);
 };
