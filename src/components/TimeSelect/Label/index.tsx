65880bfdfd30b51a4985e37c232861a0bc4fe680
Sergey Bocharov <Sergei-b84@bk.ru>
2025-10-29 15:35:08 +0000

    Fix TimeSelect

--- src/components/TimeSelect/Label/index.tsx
+++ src/components/TimeSelect/Label/index.tsx
@@ -1,20 +1,19 @@
-import React, { useRef } from "react";
+import React from "react";
 import type { FC } from "react";
 
 type OptionLabelProps = {
-  time: string;
-  onClick?: () => void;
-  onMouseEnter?: () => void;
+	time: string;
+	onClick?: () => void;
+	onMouseEnter?: () => void;
 };
 
 const OptionLabel: FC<OptionLabelProps> = ({ time, onClick, onMouseEnter }) => {
-  const ref = useRef<null | HTMLLIElement>(null);
-  return (
-    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
-    <span onClick={onClick} onMouseEnter={onMouseEnter} ref={ref}>
-      {time}
-    </span>
-  );
+	return (
+		// eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+		<span onClick={onClick} onMouseEnter={onMouseEnter}>
+			{time}
+		</span>
+	);
 };
 
 export default OptionLabel;
